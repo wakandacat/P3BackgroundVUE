b883b5f2a6d4c8198f22e9a4d649256553e57c0e
Ellena Tzavelas <wakandacat@gmail.com>
2025-05-02 16:24:05 -0400

    dev comments

--- src/main.js
+++ src/main.js
@@ -7,3 +7,9 @@ app.mount('#app');
 
 // Add class to body after Vue instance is mounted
 document.body.classList.add('bodyClass');
+
+//TO DEPLOY TO GITHUB PAGES
+//'npm run build' on main branch to update dist folder
+//'git add dist -f'
+//'git commit -m "message"'
+//'git subtree push --prefix dist origin pages-output' to migrate dist updates to pages-output branch which will rerun github action
